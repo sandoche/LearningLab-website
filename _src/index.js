5e99621c503b798c732cfb954ef7b045b943c44c
Sandoche Adittane <sandoche@gmail.com>
2018-02-28 22:54:44 +0100

    Modal created

--- _src/index.js
+++ _src/index.js
@@ -1,2 +1,20 @@
 // CSS and SASS files
 import './index.scss';
+
+(function() {
+  var updateButton = document.getElementById('updateDetails');
+  var cancelButton = document.getElementById('cancel');
+
+  // Update button opens a modal dialog
+  updateButton.addEventListener('click', function() {
+    document.getElementById('favDialog').showModal();
+    document.getElementById('favDialog').classList.add('dialog-scale');
+  });
+
+  // Bouton pour fermer la boîte de dialogue
+  cancelButton.addEventListener('click', function() {
+    document.getElementById('favDialog').close();
+    document.getElementById('favDialog').classList.remove('dialog-scale');
+  });
+
+})();
