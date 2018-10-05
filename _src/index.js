bdbae33a429398d095f954a6fbed40097b38121b
Sandoche Adittane <sandoche@gmail.com>
2018-10-05 21:28:57 +0200

    :sparkles: Dynamic forms managed

--- _src/index.js
+++ _src/index.js
@@ -12,14 +12,27 @@ const modal = new tingle.modal({
 
 const modalLinks = document.getElementsByClassName('modal-link');
 const form = document.getElementById('netlify-form');
+const inputLabels = {
+  'resource': 'Resource URL',
+  'experience': 'Experience URL of blog post',
+  'project idea': 'Describe the project idea'
+};
+
+modal.setContent(
+  '<h2 id="modal-title"></h2>'
+);
 
 for (let i = 0; i < modalLinks.length; i++) {
   modalLinks[i].addEventListener("click", function() {
     const title = modalLinks[i].getAttribute('title');
-    modal.setContent(
-      '<h2 id="modal-title">New '+ title +'</h2>'
-    );
-    const modalTitle = document.getElementById('modal-title');
+
+    let modalTitle = document.getElementById('modal-title');
+    let inputType = document.getElementById('form-input-type');
+    let inputLabel = document.getElementById('form-input-label');
+    modalTitle.innerHTML = 'Add ' + title;
+    inputType.value = title;
+    inputLabel.innerHTML = inputLabels[title];
+
     modalTitle.after(form);
     form.style.display = 'block';
     modal.open();
